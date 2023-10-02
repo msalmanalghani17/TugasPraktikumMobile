a71b51b33f620fd9186214b39e654e7421e1a00a
msalmanalghani17 <145442975+msalmanalghani17@users.noreply.github.com>
2023-10-02 17:17:59 +0700

    Tugas1PraktikumMobile

---
+++ tugas2.dart
@@ -0,0 +1,9 @@
+void main() {
+  String a = "Muhammad";
+  String b = "Salman";
+  String c = "Al-Ghani";
+  String d = "Kelas C 2021";
+  String e = "Prodi Informatika";
+
+  print("${a} + ${b} + ${c} + ${d} + ${e}");
+}
